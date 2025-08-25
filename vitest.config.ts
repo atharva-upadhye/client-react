f897b1194c31cb66dee7eac8ba0ff03446dd26a6
Atharva Upadhye <70993547+atharva-upadhye@users.noreply.github.com>
2025-08-25 17:06:46 +0530

    config: simplify vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig, defaultExclude } from "vitest/config";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    root: "./src",
     environment: "jsdom",
     globals: true,
     setupFiles: "./vitest.setup.ts",
-    exclude: [...defaultExclude, "**/e2e/**"],
   },
 });
