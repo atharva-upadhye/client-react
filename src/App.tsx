50f424a37b74cf1a23662ea089d126cd120f5f17
Atharva Upadhye <70993547+atharva-upadhye@users.noreply.github.com>
2025-08-29 21:45:26 +0530

    error boundary + react lazy, suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,45 @@
 import "./App.css";
+import { Suspense, lazy, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "./components/ErrorFallback/ErrorFallback";
 import Timer from "./components/Timer/Timer";
 
+const LazyComponent = lazy(() =>
+  import("./components/LazyComponent/LazyComponent").then(e => ({
+    default: e.LazyComponent,
+  })),
+);
+
+function ComponentThatMayThrow({ userId }: { userId: string }) {
+  if (userId === "0") {
+    throw new Error("Invalid user!");
+  }
+  return <div>Valid user: {userId}</div>;
+}
+
 function App() {
+  const [userId, setUserId] = useState("1");
+
   return (
-    <div className="m-auto w-fit">
-      <div className="text-2xl">timer app</div>
-      <Timer />
-    </div>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => setUserId("1")} // Reset logic
+      onError={(error, info) => {
+        // Send to monitoring service (e.g., Sentry)
+        console.error("Logged error:", error, info);
+      }}
+    >
+      <ComponentThatMayThrow userId={userId} />
+      <br />
+      <Suspense fallback={<>loading...</>}>
+        <LazyComponent />
+      </Suspense>
+      <br />
+      <div className="m-auto w-fit">
+        <div className="text-2xl">timer app</div>
+        <Timer />
+      </div>
+    </ErrorBoundary>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import "./index.css";
-import App from "./App.tsx";
+import App from "./App";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root");
+if (!rootElement)
+  throw new Error(
+    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
+  );
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
