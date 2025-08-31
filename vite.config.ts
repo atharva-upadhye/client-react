017691ccd2bc308c8149787b4fb8a4035d6dc59b
Atharva Upadhye <70993547+atharva-upadhye@users.noreply.github.com>
2025-08-31 06:39:43 +0530

    add vite checker for ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,25 @@
+import checker from "vite-plugin-checker";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
   build: {
     rollupOptions: {
-      treeshake: "recommended", // TODO: verify if this is working
+      // eslint-disable-next-line no-warning-comments
+      // TODO: verify if this is working
+      treeshake: "recommended",
     },
+  },
+  plugins: [
+    react(),
+
+    checker({
+      typescript: {
+        tsconfigPath: "./tsconfig.app.json",
+      },
+    }),
+  ],
+  server: {
+    port: 3000,
   },
 });
