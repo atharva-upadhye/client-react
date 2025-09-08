8bbb27d6ec9499ef578ea4d7f76cf617a844cb1c
Atharva Upadhye <70993547+atharva-upadhye@users.noreply.github.com>
2025-09-08 10:05:31 +0530

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "client-react",
   build: {
     rollupOptions: {
       // eslint-disable-next-line no-warning-comments
